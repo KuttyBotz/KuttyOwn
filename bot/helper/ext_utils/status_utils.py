718b50c9b13d059b7b1d81f676af93ed488b76c2
Kutty T <149066597+KuttyBotz@users.noreply.github.com>
2025-09-19 16:14:00 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -107,16 +107,13 @@ def speed_string_to_bytes(size_text: str):
     return size
 
 
-def get_progress_bar_string(pct: str):
-    pct = float(pct.strip('%'))
+def get_progress_bar_string(pct):
+    pct = float(str(pct).strip("%"))
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
-    p_str = '⬤' * cFull
-    if cPart >= 0:
-        p_str += ['○', '○', '◔', '◔', '◑', '◑', '◕', '◕'][cPart]
-    p_str += '○' * (12 - cFull)
-    return f"[{p_str}]"
+    p_str = "⬢" * cFull
+    p_str += "⬡" * (12 - cFull)
+    return f"{p_str}"
 
 
 def action(message: Message):
